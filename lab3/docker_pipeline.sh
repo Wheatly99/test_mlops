11b6b4f95610342b5da0407a67e3ea4a8fc3a4c5
Wheatly99 <99mmarkin@gmail.com>
2024-03-28 22:17:17 +0500

    Add file

---
+++ lab3/docker_pipeline.sh
@@ -0,0 +1,2 @@
+docker build -t lab3 .
+docker run --rm --name lab3 lab3
---
+++ lab3/model_testing.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import joblib
+
+def predict():
+
+    df_test = pd.read_csv('test/test_data.csv', sep='~')
+
+    model = joblib.load('model.pkl')
+
+    pred = model.predict(df_test.drop(columns={'target'}))
+
+    print(f'Accuracy - {accuracy_score(pred, df_test.target)}')
+
+if __name__ == '__main__':
+    predict()
