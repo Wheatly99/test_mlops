f34160abfe5249623e87abac3f258b813d057515
Wheatly99 <99mmarkin@gmail.com>
2024-05-01 15:33:59 +0500

    Add loop

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 data_creation.py
-python3 data_preprocessing.py
-python3 model_preparation.py
-python3 model_testing.py
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
