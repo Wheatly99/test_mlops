a5259c035abc62721a2a83b846a3fcf5599ed188
Wheatly99 <99mmarkin@gmail.com>
2024-05-01 14:20:29 +0500

    add file

---
+++ lab1/model_preparation.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LogisticRegression
+import joblib
+import pandas as pd
+
+def create_model():
+
+    df_train = pd.read_csv('train/train_data.csv', sep='~')
+
+    model = LogisticRegression(random_state=42)
+    model.fit(df_train.drop(columns={'target'}), df_train.target)
+    joblib.dump(model, 'model.pkl')
+
+if __name__ == '__main__':
+    create_model()
